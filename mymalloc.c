93e70b86905c39c9d785b3557ddb0ee1da49e025
Will <williamht123@yahoo.com>
2024-02-17 19:12:49 -0500

    init

---
+++ mymalloc.c
@@ -0,0 +1,15 @@
+#include "mymalloc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static double memory[MEMLENGTH];
+
+void *mymalloc(unsigned int size, char *FILE, int LINE){
+
+
+}
+
+void myfree(void *z, char *FILE, int LINE){
+
+
+}
--- mymalloc.h
+++ mymalloc.h
@@ -1,10 +1,13 @@
+#ifndef _mymalloc_h_
 #define _mymalloc_h_
 
 #include <stdlib.h>
 #include <stdio.h>
 
+#define MEMLENGTH 512
+
 #define malloc(s) mymalloc(s, __FILE__, __LINE__);
 #define free(p) free(p, __FILE__, __LINE__);
 
-void *mymalloc(unsigned int size, char *file, int line);
-void myfree(void *p, char *file, int line);
+void *mymalloc(unsigned int size, char *FILE, int LINE);
+void myfree(void *z, char *FILE, int LINE);
